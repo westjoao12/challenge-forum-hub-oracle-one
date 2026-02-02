c09b636c289f565d3efdca39c5fd1de7cb50e530
westjoao12 <westjoao12@gmail.com>
2026-02-02 05:37:58 -0300

    seed

---
+++ src/main/resources/db/migration/V5__insert-test-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO cursos (nome, categoria)
+VALUES ('Java Spring Boot', 'Programação');
+
+INSERT INTO cursos (nome, categoria)
+VALUES ('Java e Orientação a Objeto', 'Programação');
+
+-- Hash BCrypt para a senha '123456'
+INSERT INTO usuarios (nome, email, senha)
+VALUES ('Test User', 'test@forumhub.com', '$2a$10$QfQQBeZ9WUddoRENqpygAecheaWGf.IKR.jThNeETzsm9PtLT8sr2');
