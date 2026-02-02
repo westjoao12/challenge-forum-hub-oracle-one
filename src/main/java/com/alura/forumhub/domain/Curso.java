f193417b35259df3d7ddb3d22504e18fdfbe2777
westjoao12 <westjoao12@gmail.com>
2026-02-01 22:40:20 -0300

    feat: modeling Curso's Entity

---
+++ src/main/java/com/alura/forumhub/domain/Curso.java
@@ -0,0 +1,22 @@
+package com.alura.forumhub.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "cursos")
+@Entity(name = "Curso")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String categoria;
+}
