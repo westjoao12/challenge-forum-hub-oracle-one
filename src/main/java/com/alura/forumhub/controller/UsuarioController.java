8c8386112eb605f8914d786b8a0121acd714367d
westjoao12 <westjoao12@gmail.com>
2026-02-02 02:56:12 -0300

    feat: user registration

--- src/main/java/com/alura/forumhub/controller/UsuarioController.java
+++ src/main/java/com/alura/forumhub/controller/UsuarioController.java
@@ -1,4 +1,29 @@
 package com.alura.forumhub.controller;
 
+import com.alura.forumhub.domain.usuario.DadosCadastroUsuario;
+import com.alura.forumhub.service.UsuarioService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/usuarios")
+@Tag(name = "3. Usuários")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroUsuario dados, UriComponentsBuilder uriBuilder, jakarta.servlet.http.HttpServletRequest request) {
+        return service.cadastrar(dados, uriBuilder, request);
+    }
 }
--- src/main/java/com/alura/forumhub/service/UsuarioService.java
+++ src/main/java/com/alura/forumhub/service/UsuarioService.java
@@ -1,4 +1,44 @@
 package com.alura.forumhub.service;
 
+import com.alura.forumhub.domain.usuario.DadosCadastroUsuario;
+import com.alura.forumhub.domain.usuario.DadosListagemUsuario;
+import com.alura.forumhub.domain.usuario.Usuario;
+import com.alura.forumhub.infra.exception.DadosErroDefault;
+import com.alura.forumhub.repositories.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroUsuario dados, UriComponentsBuilder uriBuilder, jakarta.servlet.http.HttpServletRequest request){
+        // Validação: E-mail duplicado
+        if(repository.existsByEmail(dados.email())){
+            var erro = new DadosErroDefault(
+                    400,
+                    "Bad Request",
+                    "Já existe um usuário cadastrado com este e-mail",
+                    request.getRequestURI()
+
+            );
+            return ResponseEntity.badRequest().body(erro);
+        }
+
+        var senhaCriptografada = passwordEncoder.encode(dados.senha());
+        var usuario = new Usuario(null, dados.nome(), dados.email(), senhaCriptografada);
+        repository.save(usuario);
+
+        var uri = uriBuilder.path("usuarios/{id}").buildAndExpand(usuario.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosListagemUsuario(usuario));
+    }
 }
