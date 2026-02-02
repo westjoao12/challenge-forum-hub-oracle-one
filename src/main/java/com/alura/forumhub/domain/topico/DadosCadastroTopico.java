5710557984f4214effa3cdfb6defd61ca2e23659
westjoao12 <westjoao12@gmail.com>
2026-02-01 23:21:06 -0300

    feat: create DTO for Topico's creation

---
+++ src/main/java/com/alura/forumhub/domain/topico/DadosCadastroTopico.java
@@ -0,0 +1,19 @@
+package com.alura.forumhub.domain.topico;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosCadastroTopico(
+        @NotBlank(message = "Título é obrigatório")
+        String titulo,
+
+        @NotBlank(message = "Mensagem é obrigatória")
+        String mensagem,
+
+        @NotNull(message = "ID do autor é obrigatório")
+        Long idAutor,
+
+        @NotNull(message = "ID do curso é obrigatório")
+        Long idCurso
+) {
+}
